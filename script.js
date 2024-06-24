da1fbbbac1c794a0c2e011a95135cc7d9e0b3fb3
Hassan Mef <hassanmef19@gmail.com>
2024-06-24 17:41:08 +0500

    short break

--- script.js
+++ script.js
@@ -113,7 +113,120 @@ var Game = {
     this.context.fillStyle = this.color ;
 
     //Draw the retangle behind the " Press any key to begin"
-  }
+
+    this.context.fillRect(
+        this.cannvas.width / 2 -350 ,
+        this.cannvas.height / 2 -48,
+        700,
+        100
+    );
+
+    // Change the canvas color
+    this.context.fillStyle = '#ffffff';
+
+    // Draw the menu text ( " Press any key to begin " )
+    this.context.fillText(
+        'Press any key to begin',
+        this.cannvas.width / 2,
+        this.cannvas.height / 2 + 15
+    );
+
+  },
+
+  // Update all objects 
+
+  update : function() {
+    if(!this.over ){
+
+        // if the ball collides with bound limits   
+
+        if(this.ball.x <=0 ) Pong._resetTurn.call(this, this.ai , this.player);
+        if(this.ball.x >= this.cannvas.width - this.ball.width ) Pong._resetTurn.call(this, this.player, this.ai);
+        if(this.ball.y <=0 ) this.ball.moveY = DIRECTION.DOWN;
+        if(this.ball.y >= this.cannvas.height - this.ball.height ) this.ball.moveY = DIRECTION.UP;
+
+        // move player if they move value was update by a keyboard event
+
+        if(this.player.move === DIRECTION.UP) this.player.y -= this.player.speed ;
+        else if(this.player.move === DIRECTION.DOWN) this.player.y += this.player.speed ;
+
+        // On new server (start of each turn ) move bal to the correct side 
+        
+        if (  Pong._turnDelayIsOver.call(this) && this.turn){
+            this.ball.moveX = this.turn === this.player ? DIRECTION.LEFT : DIRECTION.RIGHT ;
+            this.ball.moveY = [DIRECTION.UP , DIRECTION.DOWN][Math.round(Math.random() )] ;
+            this.ball.y =Math.floor(Math.random() * this.cannvas.height -200 ) +200 ;
+            this.turn = null;
+            
+        }
+
+        //if player collides with bound limits , update the x and y coords,
+
+        if(this.player.y <= 0) this.player.y = 0 ;
+        else if (this.player.y >= (this.canvas.height -this.player.height)) this.player.y = (this.canvas.height - this.player.height);
+
+        // Move ball in intended direction based on moveY and moveX
+
+        if(this.ai.y > this.ball.y - (this.ai.height /2)) {
+            if(this.ball.moveX ===DIRECTION.RIGHT) this.ai.y -= this.ai.speed /1.5 ;
+            else this.ai.y -= this.ai.speed / 4 ;
+        }
+
+        if(this.ai.y < this.ball.y - (this.ai.height /2)) {
+            if(this.ball.moveX ===DIRECTION.RIGHT) this.ai.y += this.ai.speed /1.5 ;
+            else this.ai.y += this.ai.speed / 4 ;
+        }
+
+        // Handle ai wall collision 
+
+        if(this.ai.y >= this.canvas.height- this.ai.height) this.ai.y = this.cannvas.height -this.ai.height;
+        else if (this.ai.y <= 0 ) this.ai.y =0 ;
+
+        //Handle Player ball collisions
+
+        if(this.ball.x - this.ball.width <= this.player.x && this.ball.x >= this.player.x - this.player.width) {
+            if(this.ball.y <= this.player.y +this.player.height && this.ball.y + this.ball.height >= this.player.y){
+                this.ball.x = (this.player.x +this.ball.width);
+                this.ball.moveX = DIRECTION.RIGHT ;
+            }
+        }
+
+        // Handle ai ball collision 
+
+        if(this,ball.x -this.ball.width <=  this.ai.x && this.ball.x >= this.ai.x -this.ai.width) {
+            if(this.ball.y <= this.ai.y +this.ai.height && this.ball.y +this.ball.height >= this.ai.y) {
+                this.ball.x = (this.ai.x +this.ball.width);
+                this.ball.moveX = DIRECTION.LEFT ;
+    
+            }
+        }
+
+
+        // Handle the end of round transition 
+        // Check to see of the player won the round 
+
+        if(this.player.score === rounds[this.round]) {
+            // Check to see if there are any more rounds left and display the victory screen if there are not left 
+
+            if(!round[this.round +1]) {
+                this.over = true ;
+                setTimeout(function () { Pong.endGameMenu('Winner!'); } ,100 ) ;
+
+            }else {
+                
+            }
+        }
+
+
+
+
+
+
+
+
+    }
+
+
 
 
 
